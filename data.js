2b5e76c2fc47f7bde6ac158b2544b86073eb68da
quintonbrogan-cloud <quintonbrogan@gmail.com>
2026-03-28 11:46:45 -0400

    Update data.js

--- data.js
+++ data.js
@@ -16,56 +16,21 @@ const PORTFOLIO = {
     { slug: "spinbrush", title: "Spinbrush", video: "https://fast.wistia.net/embed/iframe/d5ww5vfasl", type: "wistia" },
     { slug: "sherlock-homes", title: "Sherlock Homes", video: "https://www.youtube.com/embed/dRBpizr3PCM", type: "youtube" },
     { slug: "defy-the-odds", title: "Defy The Odds", video: "https://player.vimeo.com/video/667398721", type: "vimeo" },
-    { slug: "brave-girl-embrace-grace", title: "'Brave Girl' Embrace Grace", video: "https://player.vimeo.com/video/861404679", type: "vimeo" },
+    { slug: "brave-girl-embrace-grace", title: "Brave Girl Embrace Grace", video: "https://player.vimeo.com/video/861404679", type: "vimeo" },
     { slug: "aaa", title: "AAA", video: "https://player.vimeo.com/video/639342583", type: "vimeo" },
-    { slug: "marshalls-activewear-15s", title: 'Marshalls "Activewear" 15s', video: "https://player.vimeo.com/video/867277742", type: "vimeo" }
+    { slug: "marshalls-activewear-15s", title: "Marshalls Activewear 15s", video: "https://player.vimeo.com/video/867277742", type: "vimeo" }
   ],
   musicVideo: [
     { slug: "sierra-spirit-ghost-official-music-video", title: "Sierra Spirit - ghost (Official Music Video)", video: "https://www.youtube.com/embed/FIBOxj25jaE", type: "youtube" },
-    { slug: "spittn-chiclets", title: "Spittn' Chiclets", video: "https://www.youtube.com/embed/pBGodH4bx3Y", type: "youtube" },
+    { slug: "spittn-chiclets", title: "Spittn Chiclets", video: "https://www.youtube.com/embed/pBGodH4bx3Y", type: "youtube" },
     { slug: "dead-emerson-eleventh-hour", title: "Dead Emerson - Eleventh Hour", video: "https://www.youtube.com/embed/Yxu_84QCbkA", type: "youtube" },
     { slug: "karma-a-will-away", title: "Karma - A Will Away", video: "https://www.youtube.com/embed/s5g-DCxmX20", type: "youtube" },
     { slug: "des-rocks-imaginary-friends", title: "Des Rocks - Imaginary Friends", video: "https://www.youtube.com/embed/BhRvU9IqQpU", type: "youtube" },
-    { slug: "mitchel-dae-innocent-live", title: "Mitchel Dae -Innocent live", video: "https://www.youtube.com/embed/HkgAchzbrHY", type: "youtube" }
+    { slug: "mitchel-dae-innocent-live", title: "Mitchel Dae - Innocent live", video: "https://www.youtube.com/embed/HkgAchzbrHY", type: "youtube" }
   ],
   doc: [
     { slug: "chris-brickley-x-red-bull", title: "Chris Brickley x Red Bull", video: "https://player.vimeo.com/video/372786655", type: "vimeo" },
     { slug: "new-tribe-rising", title: "New Tribe Rising", video: "https://player.vimeo.com/video/286334354", type: "vimeo" },
     { slug: "discover-the-tuscan-olive-trees-growing-in-california", title: "Discover the Tuscan Olive Trees Growing in California", video: "https://www.youtube.com/embed/dDVbwwbh05A", type: "youtube" }
   ]
-};const PORTFOLIO = {
-    commercial: [
-      { slug: "shokz-directors-cut", title: "Shokz (directors cut)", video: "https://www.youtube.com/embed/7nTOkXUMWZY", type: "youtube" },
-      { slug: "asco-sound-of-progress", title: "ASCO - Sound of Progress", video: "https://player.vimeo.com/video/710483743", type: "vimeo" },
-      { slug: "best-buy", title: "Best Buy", video: "https://fast.wistia.net/embed/iframe/pv6df2jktj", type: "wistia" },
-      { slug: "pga2k-nsw2", title: "PGA2K- Tiger", video: "https://player.vimeo.com/video/1150499022", type: "vimeo" },
-      { slug: "connecticut-lottery-keno-yeah", title: "CONNECTICUT LOTTERY KENO YEAH", video: "https://player.vimeo.com/video/1013596748", type: "vimeo" },
-      { slug: "ad-council-buzzed-driving-is-drunk-driving-30", title: "Ad Council - Buzzed Driving is Drunk Driving - 30", video: "https://player.vimeo.com/video/785102195", type: "vimeo" },
-      { slug: "delloitte-brand-film", title: "Delloitte Brand Film", video: "https://fast.wistia.net/embed/iframe/rihr0ex733", type: "wistia" },
-      { slug: "country-crock", title: "Country Crock", video: "https://player.vimeo.com/video/696812977", type: "vimeo" },
-      { slug: "dietz-and-watson", title: "Dietz and Watson", video: "https://player.vimeo.com/video/630572467", type: "vimeo" },
-      { slug: "best-buy-mess-up", title: "Best Buy - Mess Up", video: "https://fast.wistia.net/embed/iframe/fbggvtws70", type: "wistia" },
-      { slug: "get-there-florida-education", title: "Get There - Florida Education", video: "https://player.vimeo.com/video/617113397", type: "vimeo" },
-      { slug: "microsoft-edge-holiday", title: "Microsoft Edge Holiday", video: "https://fast.wistia.net/embed/iframe/vp6voe2xso", type: "wistia" },
-      { slug: "ryerson-insurance", title: "Ryerson Insurance", video: "https://player.vimeo.com/video/639725079", type: "vimeo" },
-      { slug: "spinbrush", title: "Spinbrush", video: "https://fast.wistia.net/embed/iframe/d5ww5vfasl", type: "wistia" },
-      { slug: "sherlock-homes", title: "Sherlock Homes", video: "https://www.youtube.com/embed/dRBpizr3PCM", type: "youtube" },
-      { slug: "defy-the-odds", title: "Defy The Odds", video: "https://player.vimeo.com/video/667398721", type: "vimeo" },
-      { slug: "brave-girl-embrace-grace", title: "'Brave Girl' Embrace Grace", video: "https://player.vimeo.com/video/861404679", type: "vimeo" },
-      { slug: "aaa", title: "AAA", video: "https://player.vimeo.com/video/639342583", type: "vimeo" },
-      { slug: "marshalls-activewear-15s", title: "Marshalls Activewear 15s", video: "https://player.vimeo.com/video/867277742", type: "vimeo" }
-        ],
-    musicVideo: [
-      { slug: "sierra-spirit-ghost-official-music-video", title: "Sierra Spirit - ghost (Official Music Video)", video: "https://www.youtube.com/embed/FIBOxj25jaE", type: "youtube" },
-      { slug: "spittn-chiclets", title: "Spittn Chiclets", video: "https://www.youtube.com/embed/pBGodH4bx3Y", type: "youtube" },
-      { slug: "dead-emerson-eleventh-hour", title: "Dead Emerson - Eleventh Hour", video: "https://www.youtube.com/embed/Yxu_84QCbkA", type: "youtube" },
-      { slug: "karma-a-will-away", title: "Karma - A Will Away", video: "https://www.youtube.com/embed/s5g-DCxmX20", type: "youtube" },
-      { slug: "des-rocks-imaginary-friends", title: "Des Rocks - Imaginary Friends", video: "https://www.youtube.com/embed/BhRvU9IqQpU", type: "youtube" },
-      { slug: "mitchel-dae-innocent-live", title: "Mitchel Dae -Innocent live", video: "https://www.youtube.com/embed/HkgAchzbrHY", type: "youtube" }
-        ],
-    doc: [
-      { slug: "chris-brickley-x-red-bull", title: "Chris Brickley x Red Bull", video: "https://player.vimeo.com/video/372786655", type: "vimeo" },
-      { slug: "new-tribe-rising", title: "New Tribe Rising", video: "https://player.vimeo.com/video/286334354", type: "vimeo" },
-      { slug: "discover-the-tuscan-olive-trees-growing-in-california", title: "Discover the Tuscan Olive Trees Growing in California", video: "https://www.youtube.com/embed/dDVbwwbh05A", type: "youtube" }
-        ]
 };
